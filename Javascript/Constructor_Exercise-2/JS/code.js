d35d5ab85401db11a1fa3b35c92fe72414bf69d4
Brogy-alt <brogangys2@gmail.com>
2022-11-30 15:52:43 +0200

    TEll ME WHY!!

---
+++ Javascript/Constructor_Exercise-2/JS/code.js
@@ -0,0 +1,81 @@
+
+// let personDetails= {
+//     // data property
+//     firstName: 'Peter',
+//     lastName: 'File',
+//     email: 'peterfile@gmail',
+ 
+//     // (getter)
+//     get personDetails() {
+//         return this.firstName,lastName,email; 
+//     },
+//     //(setter)
+//     set changeName(newName) {
+//         this.firstName = newName;
+//         // new name
+//         personDetails.newName = 'IsnotAPeter';
+
+//     }
+    
+// };
+
+// // callout
+// console.log(personDetails.firstName);
+// console.log(personDetails.lastName);
+// console.log(personDetails.email);
+// console.log(personDetails.newName); 
+
+    
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// // Solution 2 using a class
+// class PersonDetails{
+//     // Private
+//     #firstName;
+//     #lastName;
+//     #email;
+//     constructor(firstName, lastName, email) {
+//         this.#firstName = firstName;
+//         this.#lastName = lastName;
+//         this.#email = email;
+//     }
+//     // FirstName
+//     get firstName() {
+//         return this.#firstName;
+//     }
+//     set firstName(value) {
+//         this.#firstName = value;
+//     }
+//     // LastName
+//     get lastName() {
+//         return this.#lastName;
+//     }
+//     set lastName(value) {
+//         this.#lastName = value;
+//     }
+//     // Email
+//     get email() {
+//         return this.#email;
+//     }
+//     set email(value) {
+//         this.#email = value;
+//     }
+//     // Display
+//     display() {
+//         console.log(`
+//         Name: ${this.firstName}
+//         Surname: ${this.lastName}
+//         Email: ${this.email}`);
+//     }
+// }
+// let person1 = new PersonDetails("Joel", "Mukanya", "joel@gmail.com");
+// person1.display();
+// console.log("===========");
+// person1.firstName = "Peter";
+// person1.lastName = "Henk";
+// person1.email = "peter@gmail.com"
+// person1.display();
+
+
+
+
